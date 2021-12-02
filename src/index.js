777655555eec3b4de374f554c510e26bd8d6e6e9
Shrey Gupta <connectwithshrey@gmail.com>
2021-12-02 15:57:41 +0530

    add metricsPrefix to axios requests

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const appVersion = metricsMiddleware.exports.version;
 const projectName = metricsMiddleware.exports.name.replace(/-/g, '_');
 
 module.exports = require('./metrics-middleware')(appVersion, projectName);
-module.exports.HttpMetricsCollector = (projectName, extraLabels) => require('./request-response-collector')(projectName, extraLabels);
+module.exports.HttpMetricsCollector = (metricsPrefix, extraLabels) => require('./request-response-collector')(metricsPrefix, extraLabels);
 module.exports.koaMiddleware = require('./metrics-middleware')(appVersion, projectName, 'koa');
 module.exports.expressMiddleware = require('./metrics-middleware')(appVersion, projectName, 'express');
