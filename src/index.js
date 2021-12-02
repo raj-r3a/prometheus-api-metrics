e7ff78db59ebca836da4d3c0a539ce19a17d6cff
Shrey Gupta <connectwithshrey@gmail.com>
2021-12-02 15:49:27 +0530

    add extraLabels

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ const Path = require('path');
 const metricsMiddleware = { exports: {} };
 require('pkginfo')(metricsMiddleware, { dir: Path.dirname(module.parent.filename), include: ['name', 'version'] });
 const appVersion = metricsMiddleware.exports.version;
+const extraLabels = metricsMiddleware.exports.extraLabels;
 const projectName = metricsMiddleware.exports.name.replace(/-/g, '_');
 
 module.exports = require('./metrics-middleware')(appVersion, projectName);
-module.exports.HttpMetricsCollector = require('./request-response-collector')(projectName);
+module.exports.HttpMetricsCollector = require('./request-response-collector')(projectName, extraLabels);
 module.exports.koaMiddleware = require('./metrics-middleware')(appVersion, projectName, 'koa');
 module.exports.expressMiddleware = require('./metrics-middleware')(appVersion, projectName, 'express');
